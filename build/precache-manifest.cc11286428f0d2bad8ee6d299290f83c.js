6ce12c9c487d9bbf130b65a50bf4f6ae1ee2a783
Manuel Lopez <manuelfx117@gmail.com>
2019-12-04 21:46:53 -0600

    asdfa

---
+++ build/precache-manifest.cc11286428f0d2bad8ee6d299290f83c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8000d953cdb97b79a119ac0659e6e389",
+    "url": "/index.html"
+  },
+  {
+    "revision": "469e72a6efdccda8df29",
+    "url": "/static/css/main.f5ef3909.chunk.css"
+  },
+  {
+    "revision": "810e38da13570636d315",
+    "url": "/static/js/2.b10f5637.chunk.js"
+  },
+  {
+    "revision": "469e72a6efdccda8df29",
+    "url": "/static/js/main.51303643.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  },
+  {
+    "revision": "5100f13f635a0ff2308f269daf30a4b9",
+    "url": "/static/media/aragon.5100f13f.jpg"
+  },
+  {
+    "revision": "b9b20881c6d7b90b6eeed6afc92bd6bb",
+    "url": "/static/media/bg1.b9b20881.jpg"
+  }
+]);
